488422e9b92641429960c80125e4d793831e11fe
Yurii Mosin <m.yuriy@icloud.com>
2024-10-30 18:46:31 +0200

    make build

---
+++ index.b977f7bd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight;t.style.top="".concat((l-t.height)/2,"px"),t.style.left="".concat((c-t.width)/2,"px");
+//# sourceMappingURL=index.b977f7bd.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+// write code here
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const a = wall.clientWidth;
+const b = wall.clientHeight;
+spider.style.top = `${(b - spider.height) / 2}px`;
+spider.style.left = `${(a - spider.width) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
